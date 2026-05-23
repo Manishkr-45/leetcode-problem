acd968c6b667a1bab68305598cd63bf914a3e390
MANISH KUMAR SHARMA <iammanishkumar002@gmail.com>
2026-05-23 21:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (80.13%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int st = 0;
         int end = nums.size()-1;
         while(st<=end){
-            int mid= (st+end)/2;
+            int mid= st + (end-st)/2;
             if(target > nums[mid]){
                 st=mid+1;
             }

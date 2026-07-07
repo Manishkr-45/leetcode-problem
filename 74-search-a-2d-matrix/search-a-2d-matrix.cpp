e85e618f87ef7954e6055814239c00f385cb387d
MANISH KUMAR SHARMA <iammanishkumar002@gmail.com>
2026-07-07 17:17:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (44.82%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
+    bool searchRow(vector<vector<int>>& matrix, int target, int row){
         int n = matrix[0].size();
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        int s = 0;
+        int e = n-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(target == matrix[row][mid]){
+                return true ;
+            }else if (target > matrix[row][mid]){
+                s = mid +1;
+            }else {
+                e = mid -1;
             }
         }
         return false;
-        
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // BS ON THE ROWS
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int sr = 0;
+        int er = m-1;
+   
+        while(sr<=er){
+            int midrow = sr +(er-sr)/2;
+            if(target >= matrix[midrow][0] && target<= matrix[midrow][n-1]){
+                //found the row
+                return searchRow(matrix,target,midrow);
+
+            }
+            else if (target >matrix[midrow][n-1]){
+                sr = midrow+1;
+            }
+            else {
+                er = midrow-1;
+            }
+
+        }
+        return false;   
     }
 };

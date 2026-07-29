47d39254313649bea13d0090daf2f354c9d2ecfc
MANISH KUMAR SHARMA <iammanishkumar002@gmail.com>
2026-07-29 23:41:49 +0530

    Time: 2 ms (39.87%) | Memory: 10.3 MB (97.50%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void allSubset(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allsubsets){
+        if(i == nums.size()){
+            allsubsets.push_back(ans);
+            return;
+
+        }
+        ans.push_back(nums[i]);
+        allSubset(nums,ans,i+1,allsubsets);
+        ans.pop_back();
+        int idx = i+1;
+        while(idx < nums.size() && nums[idx] == nums[idx-1]) idx++;
+    
+        allSubset(nums,ans,idx,allsubsets);
+    }
+
+
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> allsubsets;
+        vector<int> ans;
+        allSubset(nums,ans,0,allsubsets);
+
+        return allsubsets;
+        
+    }
+};

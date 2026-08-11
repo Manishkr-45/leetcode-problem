59be82e3addd447d87e0fef344afd9eacd409e1e
MANISH KUMAR SHARMA <iammanishkumar002@gmail.com>
2026-08-11 23:03:37 +0530

    Time: 48 ms (52.01%) | Memory: 54.6 MB (65.35%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPal(string s){
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        return s == s2;
+
+    }
+    void getAllparts(string s, vector<string> &partition, vector<vector<string>> &ans){
+        if(s.size()==0){
+            ans.push_back({partition});
+            return;
+        }
+        for(int i = 0; i<s.size(); i++){
+            string part = s.substr(0,i+1);
+            if(isPal(part)){
+                partition.push_back(part);
+                getAllparts(s.substr(i+1),partition,ans);
+                partition.pop_back();
+            }
+        }
+
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string> partition;
+        vector<vector<string>> ans;
+        getAllparts(s,partition,ans);
+        return ans;
+        
+    }
+};

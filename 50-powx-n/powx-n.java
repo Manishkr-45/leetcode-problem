bef86812502aba8c5b224992cfe1b8de1a18ac77
MANISH KUMAR SHARMA <iammanishkumar002@gmail.com>
2026-09-22 15:11:45 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (89.15%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long power = n;
+        if(power<0){
+            x = 1/x;
+            power = -power;
+        }
+        while(power>0){
+            if(power%2 !=0){
+                ans *= x;
+            }
+            x *= x;
+            power = power/2;
+        }
+        return ans;
+        
+    }
+}
